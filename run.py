eb6e47468fc4d68e5f8a3888f7c98939e8936d0b
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-09-22 09:03:10 +0000

    Add user request loop to repeat request for data until data provided is valid

--- run.py
+++ run.py
@@ -13,15 +13,22 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
-    """get sales figures input from the user"""
-    print("please input sales data from the last market.")
-    print("Data should be six numbers separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    """
+    get sales figures input from the user
+    """
+    while True:
+        print("please input sales data from the last market.")
+        print("Data should be six numbers separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
     
-    data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is Valid")
+            break
+    return sales_data
 
 def validate_data(values):
     """
@@ -37,7 +44,10 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+    
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
 
